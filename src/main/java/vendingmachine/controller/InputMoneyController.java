361d1a4589feabf09cca1fc89fefb873c3f417fa
yangdongjue5510 <djwhy5510@gmail.com>
2021-12-09 15:02:50 +0900

    feat: 사용자에게 숫자를 입력받는다.

--- src/main/java/vendingmachine/controller/InputMoneyController.java
+++ src/main/java/vendingmachine/controller/InputMoneyController.java
@@ -1,9 +1,15 @@
 package vendingmachine.controller;
 
+import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.view.InputMoneyView;
 
 public class InputMoneyController {
 	public void control() {
 		InputMoneyView.messageInputMoney();
+		inputMoney();
+	}
+
+	private long inputMoney() {
+		return Long.parseLong(Console.readLine());
 	}
 }
